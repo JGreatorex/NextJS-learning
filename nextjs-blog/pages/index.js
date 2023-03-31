26a75bbeb481444662f7f66bef6aeb075d346aa3
Jamie Greatorex <jamie.greatorex99@gmail.com>
2023-03-31 15:45:16 +0100

    Added alert module, tailwind, sass etc

--- nextjs-blog/pages/index.js
+++ nextjs-blog/pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head';
 import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
+import Link from 'next/link';
 
 export default function Home() {
   return (
@@ -11,6 +12,14 @@ export default function Home() {
         <section className={utilStyles.headingMd}>
             <p>Hello, I'm <strong>Jamie</strong>. I'm a Web developer. You can contact me on <a href="https://www.twitter.com/jamiegreatorex" target="_blank">Twitter</a>.</p>
             <p>This is a sample site built with <a href="https://nextjs.org" target="_blank">Next.js</a>.</p>
+            <h2>Check out my latest blog posts below</h2>
+            <ul>
+                <li>
+                    <Link href="/posts/first-post">
+                        First post
+                    </Link>
+                </li>
+            </ul>
         </section>
       </Layout>
   );

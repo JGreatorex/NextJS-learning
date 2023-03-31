02d0a7a02f33cc6dc873c0c6b7caf24caad08c25
Jamie Greatorex <jamie.greatorex99@gmail.com>
2023-03-31 14:00:59 +0100

    Converting to npm next js project
    https://nextjs.org/learn/foundations/from-react-to-nextjs/getting-started-with-nextjs

---
+++ index.jsx
@@ -0,0 +1,32 @@
+import { useState } from 'react';
+
+function Header({ title }) {
+    return <h1>{title ? title: "Default title"}</h1>;
+}
+
+function HomePage() {
+    const [likes, setLikes] = React.useState(0);
+
+
+    const names = ['Jamie Greatorex', 'James Lee', 'Joshua Serpis'];
+
+    function handleClick() {
+        setLikes(likes + 1);
+    }
+
+    return (
+        <div>
+            {/* Nesting the Header component */}
+            <Header title="React!"/>
+            <ul>
+                {names.map((name) => (
+                    <li key={name}>{name}</li>
+                ))}
+            </ul>
+
+            <button onClick={handleClick}>Like({likes})</button>
+        </div>
+    )
+}
+
+ReactDOM.render(<HomePage />, app)
